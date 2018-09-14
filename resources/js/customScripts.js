8332946f24c5fc7bc7427b685e0d2a4dcb994cc2
ArmandoTorres <ing.armandotorres@gmail.com>
2018-09-14 07:58:58 -0400

    Add menu listener

--- resources/js/customScripts.js
+++ resources/js/customScripts.js
@@ -1,5 +1,23 @@
 function loadDocument() {
+	addMenuListeners();
+}
 
-
-
+function addMenuListeners() {
+	var menuElement = document.getElementById('page-container.page-sidebar.menu-list');
+	if (menuElement !== undefined) {
+		menuElement.addEventListener("click", (e) => {
+			let menuItems = menuElement.getElementsByTagName('li');
+			for (let i = 0; i < menuItems.length; i++) {
+				if (menuItems[i].classList.contains('open')) {
+					menuItems[i].classList.remove('open');
+				}
+			}
+			let parentElement = e.target.parentNode;
+			if (parentElement.nodeName == 'LI') {
+				parentElement.classList.add('open')	
+			} else {
+				parentElement.parentNode.classList.add('open')
+			}
+		});
+	}
 }
